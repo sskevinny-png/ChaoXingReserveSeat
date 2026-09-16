21b2f4ee9f8e86581ba16c242eabd13289a3e571
sskevinny-png <sskevinny@gmail.com>
2026-09-16 19:46:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ get_current_dayofweek = lambda action: (
 
 
 SLEEPTIME = 0.2  # 每次抢座的间隔
-ENDTIME = "18:40:00"  # 根据学校的预约座位时间+1min即可
+ENDTIME = "19:55:00"  # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = True  # 是否有滑块验证
 MAX_ATTEMPT = 5  # 最大尝试次数
-RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
+RESERVE_NEXT_DAY = True  # 预约明天而不是今天的
 
 
 def login_and_reserve(users, usernames, passwords, action, success_list=None):
